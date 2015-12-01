31fa71aba670e2affee75ac42ce30a8b7ecb7c84
sydneyhodge <sydneyahodge@gmail.com>
2015-12-01 00:13:40 -0500

    New views for products. Increased code reuseability. Started specific product page design.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,26 @@
 class ProductsController < ApplicationController
+	# The only pages that have views are the index, show, new, and
+	# edit have views.
+
 	def index
+		@products = Product.all
 	end
+
+	def show
+	end
+
+	def new
+	end 
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end 
 end
